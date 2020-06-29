e3b210555312b89973b09aae10ec1079b7d8180b
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-06-29 19:59:57 +0200

    Update BayesUpdate_Gaussian.py

--- BayesUpdate_Gaussian.py
+++ BayesUpdate_Gaussian.py
@@ -2,78 +2,53 @@
 Bayesian Update of a Gaussian data generation process
 """
 import numpy as np
-import scipy.stats.norm as norm
+import scipy.stats as stats
 
-class GaussianAlgo():
+class BayesUpdate_Guassian():
   """
-  The algos try to learn which Bandit arm is the best to maximize reward.
+  The algorithm updates a univariate Gaussian posterior distribution based on 
+  new data observed.
   
-  It does this by modelling the distribution of the Bandit arms with a Beta, 
-  assuming the true probability of success of an arm is Normally distributed.
+  The parameters of the Gaussian (Meand and Variance) are also modeled as 
+  Gaussian distributions.
   """
-  def __init__(self, bandit):
+  def __init__(self,prior):
     """
     Args:
-      bandit: the bandit class the algo is trying to model
+      prior: the prior probability to be used
     """
-    self.bandit = bandit
-    self.arm_count = bandit.arm_count
-    #Keep track the number of pulls for each arm because
-    #we needed to compute the mean and variance incrementally
-    self.pull_count = np.zeros(self.arm_count)
+    self.mean = 0.0
+    self.variance = 1.0
 
-    #Prior distribution of rewards for each arm is normal(0,1)
-    self.mean = np.zeros(self.arm_count)
-    self.variance = np.ones(self.arm_count)
-    #self.alpha = np.ones(self.arm_count)
-    #self.beta = np.ones(self.arm_count)
-  
-  def get_reward_regret(self, arm):
-    reward, regret = self.bandit.get_reward_regret(arm)
-    self._update_params(arm, reward)
-    return reward, regret
-  
-  def _update_params(self, arm, reward):
-    self.pull_count[arm] += 1
-    n = self.pull_count[arm]
+  def _likelihood(self, mean, variance):
+    return np.random.norm(mean,variance)
+
+  def _update(self, prior, observation):
+    """
+    Peforms a single update and returns the posterior
+    Args:
+      prior: the current probability distribution of the target variable
+    """
     
-    #Now update the mean 
+    #Now update the mean incrementally 
     #math explanation here: http://datagenetics.com/blog/november22017/index.html
-    previous_mean = self.mean[arm]
-    self.mean[arm] += (1/n) * (reward + n*previous_mean - previous_mean)
+    previous_mean = self.mean
+    self.mean += (1/n) * (observation + n*previous_mean - previous_mean)
 
     #Now update the variance incrementally
-    previous_variance += self.variance[arm]
-    self.variance[arm] += previous_variance + (reward - previous_mean)*(reward - self.mean[arm])
-    #self.alpha[arm] += reward
-    #self.beta[arm] += 1 - reward
+    previous_variance += self.variance
+    self.variance += previous_variance + (observation - previous_mean)*(observation - self.mean)
+    
+    likelihood = self._likelihood(mean,variance)
+    posterior = (likelihood * prior) / sum(posterior)
+    return posterior
 
-class BernGreedy(BetaAlgo):
-  def __init__(self, bandit):
-    super().__init__(bandit)
+### MAIN
+observation_list = [1, 0, 0.5, 0.6, 0.7, 0.5, 0.4, 0.3, 0.3, 0.5, 0.4]
+prior = np.random.beta(1,1) #uses an uninformative prior
+update_algo = BayesUpdate_Guassian(prior)
+for observation in observation_list:
+  prior = update_algo._update(prior,observation)
+  #plot(prior)
   
-  @staticmethod
-  def name():
-    return 'beta-greedy'
-   
-  def get_action(self):
-    """ Bernouilli parameters are the expected values of the beta"""
-    theta = self.alpha / (self.alpha + self.beta)
-    return theta.argmax()
-  
-class BernThompson(BetaAlgo):
-  def __init__(self, bandit):
-    super().__init__(bandit)
 
-  @staticmethod
-  def name():
-    return 'thompson'
-  
-  def get_action(self):
-    """ Bernouilli parameters are sampled from the beta"""
-    theta = np.random.beta(self.alpha, self.beta)
-    return theta.argmax()
-
-class GaussianModel():
-    def likelihood(mean,variance):
-        return np.norm(mean,variance)
