0d2f9615e0231099f070f695daa82c657bbc4b4b
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-06-29 20:00:06 +0200

    Create Simulation_BetaPriors.py

---
+++ Simulation_BetaPriors.py
@@ -0,0 +1,17 @@
+"""
+Simumlate different beta priors in order to choose the more appropriate prior
+"""
+
+class Simulation_BetaPriors():
+"""
+##Choosing a prior##
+
+#TODO
+If we have a few data points from the rewards, we can use them to choose a prior that is
+better than the uninformative prior.
+Look at https://a-little-book-of-r-for-bayesian-statistics.readthedocs.io/en/latest/src/bayesianstats.html
+
+When we know nothing, we can assume uninformative prior, a beta(1,1)
+see: https://stats.stackexchange.com/questions/297901/choosing-between-uninformative-beta-priors
+
+"""
