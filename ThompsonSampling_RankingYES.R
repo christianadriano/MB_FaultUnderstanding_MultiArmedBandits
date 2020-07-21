70854c6ac86040c292cf2c40c948808104f5f97c
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-07-22 01:26:58 +0200

    Update ThompsonSampling_RankingYES.R
    Call all arms at least once

--- ThompsonSampling_RankingYES.R
+++ ThompsonSampling_RankingYES.R
@@ -3,6 +3,9 @@ ThompsonSampling_RankingYES
 
 "
 
+library(ggplot2)
+
+
 source("C://Users//Christian//Documents//GitHub//ML_VotingAggregation//aggregateVotes.R");
 
 # Import data
@@ -17,37 +20,30 @@ failing_methods <- c(levels(unique(answerPopulation_df$FailingMethod)))
 #select one bug
 answer_df <- answerPopulation_df[answerPopulation_df$FailingMethod==failing_methods[1],]
 question_id_list <- unique(answer_df$Question.ID)
+first_question_id <- min(question_id_list)
 
-H = 100 #number of iterations (Horizon or budget)
-K = length(question_id_list) #number of arms (questions)
-questions_selected = integer(0)
-cumulative_reward_list = integer(H+1)
-cumularive_reward_list[1] = 0
+answers_per_question = 20
+K = length(question_id_list)  #number of arms (questions) starts with zero.
+H = answers_per_question * K #number of iterations (Horizon or budget, total answers obtained)
+questions_selected = integer(0);
+cumulative_reward_list = integer(H) #one reward for each iteration
 
-numbers_of_rewards_1 = integer(K) # the K above sets the initial as 10
+numbers_of_rewards_1 = integer(K) #k arms or questions
 numbers_of_rewards_0 = integer(K)
 # These two variables will be put in place in the for loops
 total_reward = 0
-for (h in 1:H) {
-  question = question_id_list[1] #maybe I do not need to set this up.
-  max_random = 0
-  #Sample and Take the arm with highest reward
-  for (k in 1:K) {
-    #sample
-    #adding 1 because they start with zero, so it is using beta(1,1) flat prior
-    random_beta = rbeta(n = 1,
-                        shape1 = numbers_of_rewards_1[k] + 1,
-                        shape2 = numbers_of_rewards_0[k] + 1) 
-    #argmax
-    if (random_beta > max_random) { 
-      max_random = random_beta
-      question = k
-    }
-  }
-  questions_selected = append(questions_selected, question)
-  #pull the best arm
-  reward = dataset[n, question] 
-  cumulative_reward_list[h+1] = cumulative_reward_list[h] + reward
+reward=0
+
+#------------------------------------------------------------------
+#Call each question once before deciding between explore/exploit
+#pull the best arm
+for(question in 1:K){
+  question_id = question + first_question_id - 1 #Convert back to the Question.ID scale
+  answer_id <- trunc(runif(n=1,min=1,max=answers_per_question))
+  reward = answer_df[answer_df$Question.ID==question_id,"Answer.reward"][answer_id] #this should done by sampling, not in order.
+  cumulative_reward_list[question] = reward
+  questions_selected <- append(questions_selected,question)
+  
   #update believe about its reward distribution
   if (reward == 1) {
     numbers_of_rewards_1[question] = numbers_of_rewards_1[question] + 1
@@ -56,17 +52,68 @@ for (h in 1:H) {
   }
   total_reward = total_reward + reward
 }
+#----------------------------------------------------------------
 
 
+#Now look for the best ARM
+for (h in 1:H) {
+  question=0
+  max_probability = 0
+  #Sample and Take the arm with highest reward
+  for (k in 1:K) {
+    #sample
+    #adding 1 because they start with zero, so it is using beta(1,1) flat prior
+    sampled_probability = rbeta(n = 1,
+                        shape1 = numbers_of_rewards_1[k] + 1,
+                        shape2 = numbers_of_rewards_0[k] + 1); 
+    #argmax
+    #continue until it finds a probability of 1 that is the highest 
+    if(sampled_probability > max_probability){
+      max_probability <- sampled_probability;
+      question <-  k;
+    }
+  }
+  questions_selected = append(questions_selected, question);
+  
+  #pull the best arm
+  question_id = question + first_question_id - 1 #Convert back to the Question.ID scale
+  answer_id <- trunc(runif(n=1,min=1,max=answers_per_question))
+  reward = answer_df[answer_df$Question.ID==question_id,"Answer.reward"][answer_id] #sample an answer
+  cumulative_reward_list[h] = cumulative_reward_list[h-1] + reward
 
+  #update believe about its reward distribution
+  if (reward == 1) {
+    numbers_of_rewards_1[question] = numbers_of_rewards_1[question] + 1
+  } else {
+    numbers_of_rewards_0[question] = numbers_of_rewards_0[question] + 1
+  }
+  total_reward = total_reward + reward
+
+}
 
 
 #-------------------------------------------------------------------------
 #Three plots: selected questions, cumulative reward, precision recall.
 
-#which question was selected at each step (nice to mark the correct ones)
+#Which question was selected at each step (nice to mark the correct ones)
+hist(questions_selected,freq=TRUE,bin=1,
+     col = 'grey',
+     main = 'Histogram of questions answered',
+     xlab = 'Questions',
+     ylab = '#answers'
+)
+
+
+ggplot(sampled_dataf,aes(sampled_dataf$Question.ID)) + 
+  geom_histogram(binwidth = 1) +
+  labs(title=paste("Answer per Question","")) +
+  labs(x="Question.ID", y="Total answers") + 
+  xlim(c(1,130));
+
 
 #cumulative rewards (number of YES)
+plot(type="l", cumulative_reward_list, xlab="iterations",ylab="reward", main="Cumulative rewards")
+
 
 #precision and recall
 
