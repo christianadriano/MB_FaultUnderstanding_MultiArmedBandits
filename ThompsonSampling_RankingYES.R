2b4ac074a6d4768c526ff0f2c2eedcef8d78ce9a
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-07-22 04:21:54 +0200

    Update ThompsonSampling_RankingYES.R

--- ThompsonSampling_RankingYES.R
+++ ThompsonSampling_RankingYES.R
@@ -21,7 +21,7 @@ summary(answerPopulation_df)
 failing_methods <- c(levels(unique(answerPopulation_df$FailingMethod)))
 
 #ranking_top
-ranking_top <- 3
+ranking_top <- 2
 failed_method <- 1
 #questionList <- c(1,4,10,14,20,23,30,32,55,56,57,58,59,72,73,77,84,92,95,97,102,104,115,119,123);
 actual_bugs <- c(1,4)+1
@@ -31,11 +31,12 @@ answer_df <- answerPopulation_df[answerPopulation_df$FailingMethod==failing_meth
 question_id_list <- unique(answer_df$Question.ID)
 first_question_id <- min(question_id_list)
 
+percentage_budget = 0.2
 answers_per_question = 20
 K = length(question_id_list)  #number of arms (questions) starts with zero.
-H = answers_per_question * K #number of iterations (Horizon or budget, total answers obtained)
+Horizon = percentage_budget*answers_per_question * K #number of iterations (Horizon or budget, total answers obtained)
 questions_selected = integer(0);
-cumulative_reward_list = integer(H) #one reward for each iteration
+cumulative_reward_list = integer(Horizon) #one reward for each iteration
 accumStatistics<- data.frame(matrix(nrow=0,ncol=5))
 colnames(accumStatistics) <- c("precision","recall","sensitivity", "accuracy", "answers");
 
@@ -67,11 +68,20 @@ for(question in 1:K){
                                             "Iteration"=question));
   
   #compute precision and recall (will be all Zero)
-  predicted_bugs=c(1000,2000)#force all negatives
-  statistics_f<- computeStatistics(predicted_bugs,actual_bugs)
-  statistics_f$answers <- question
-  accumStatistics <- rbind(accumStatistics,statistics_f)
+  #predicted_bugs=c(1000,2000)#force all negatives
+  #statistics_f<- computeStatistics(predicted_bugs,actual_bugs)
+  #statistics_f$answers <- question
+  #accumStatistics <- rbind(accumStatistics,list(0,0,0,0,question))
   
+  #obtain the total of YES for each question
+  df_agg <- aggregate(Answer.reward ~ Question.ID, data=sampled_df, sum)
+  #Sort descending
+  df_agg_sort <- df_agg[order(df_agg$Answer.reward, decreasing=TRUE),]
+  predicted_bugs <- df_agg_sort[1:ranking_top,]$Question.ID
+  statistics_f<- computeStatistics(predicted_bugs,actual_bugs); #change name of computeOutcomes to computeStat
+  statistics_f$answers <- dim(sampled_df)[1];
+  accumStatistics <- rbind(accumStatistics,statistics_f);
+
   #update believe about its reward distribution
   if (reward == 1) {
     numbers_of_rewards_1[question] = numbers_of_rewards_1[question] + 1
@@ -79,31 +89,13 @@ for(question in 1:K){
     numbers_of_rewards_0[question] = numbers_of_rewards_0[question] + 1
   }
 }
+colnames(accumStatistics) <- c("precision","recall","sensitivity", "accuracy", "answers");
+
 #----------------------------------------------------------------
-
-predictedBugs=c(1000,2000)
-actualBugs <- actual_bugs
-statistics_df <- data.frame(matrix(nrow=1,ncol=5))
-colnames(statistics_df)  <- c("precision","recall","sensitivity", "accuracy","answers");
-
-countMatch<- length(match(actualBugs,predictedBugs));
-TP <- countMatch;
-
-FP <- abs(countMatch - dim(predictedBugs)[1]);
-
-FN <- abs(countMatch - length(actualBugs));
-
-TN <- 129 - TP - FP - FN;
-
-statistics_df$precision <-  (TP/(TP+FP));
-statistics_df$recall <-  (TP/(TP+FN));
-statistics_df$sensitivity <- ((TN)/(FP+TN));
-statistics_df$accuracy <- ((TN+TP)/(FP+TN+TP+FN));
-
 
 start = K+1 #because it has already initialized K arms.
 #Now look for the best ARM
-for (h in start:H) {
+for (h in start:Horizon) {
   question=0
   max_probability = 0
   #-----------------------------
@@ -139,7 +131,7 @@ for (h in start:H) {
   #Sort descending
   df_agg_sort <- df_agg[order(df_agg$Answer.reward, decreasing=TRUE),]
   predicted_bugs <- df_agg_sort[1:ranking_top,]$Question.ID
-  statistics_f<- computeStatistics(predictedBugs,actualBugs); #change name of computeOutcomes to computeStat
+  statistics_f<- computeStatistics(predicted_bugs,actual_bugs); #change name of computeOutcomes to computeStat
   statistics_f$answers <- dim(sampled_df)[1];
   accumStatistics <- rbind(accumStatistics,statistics_f);
   #-------------
@@ -160,8 +152,6 @@ for (h in start:H) {
 #-------------------------------------------------------------------------
 #Three plots: selected questions, cumulative reward, precision recall.
 
-#TODO precision recall
-
 #Which question was selected at each step (nice to mark the correct ones)
 ggplot(sampled_df,aes(Question.ID)) + 
   geom_histogram(binwidth = 1) +
@@ -169,27 +159,51 @@ ggplot(sampled_df,aes(Question.ID)) +
   labs(x="Question.ID", y="Total answers") + 
   scale_x_continuous(breaks=seq(1,K, 1)
                      );
+#Which question gave the higher reward?
+ggplot(df_agg,aes(y=Answer.reward, x=Question.ID)) + 
+  geom_bar(stat="identity") +
+  labs(title=paste("Reward per Question - ",failing_methods[failed_method])) +
+  labs(x="Question.ID", y="Reward");
 
 #cumulative rewards (number of YES)
-plot(type="l", cumulative_reward_list, xlab="iterations",ylab="reward", main="Cumulative rewards")
-
 ggplot(sampled_df,aes(Iteration,Cumulative.reward)) + 
   geom_line() +#aes(y=100*precision, colour="precision"))+
   #geom_point(aes(x=answers,y=100*precision), shape=1) +
   labs(y="reward",x="iteration") + 
   labs(title=paste("Cumulative Reward - ",failing_methods[failed_method]));
-  
-accumStatistics_df$precision <- as.numeric(accumStatistics_df$precision)
-accumStatistics_df$recall <- as.numeric(accumStatistics_df$recall)
+
+#--------------------
+#Plot reward and answers side-by-side
+df_agg_QID <-  data.frame(table(sampled_df$Question.ID))
+colnames(df_agg_QID) <- c("Question.ID","Count")
+df_agg_QID$Type <- "Answers"
+
+colnames(df_agg) <- c("Question.ID","Count")
+df_agg$Type <- "Rewards"
+
+answer_reward_df <- data.frame(matrix(nrow=0,ncol=3))
+colnames(answer_reward_df) <- c("Question.ID","Count","Type")
+answer_reward_df <- rbind(df_agg_QID,df_agg)
+
+ggplot(answer_reward_df,aes(y=Count, x=Question.ID, fill=Type)) + 
+  geom_bar(stat="identity",position=position_dodge()) +
+  labs(title=paste("Reward and Answer by Question - ",failing_methods[failed_method])) +
+  labs(x="Question.ID", y="Count")+
+  theme_minimal()+
+  scale_fill_brewer(palette="Blues");
+
+
+#--------------------
+
 
 #precision and recall
-ggplot(accumStatistics_df,aes(answers)) + 
+ggplot(accumStatistics,aes(answers)) + 
   geom_line(aes(y=100*precision, colour="precision")) + 
   geom_line(aes(y=100*recall, colour="recall")) + 
-  geom_point(aes(x=answers,y=100*precision), shape=1) +
-  geom_point(aes(x=answers,y=100*recall), shape=1) + 
   labs(y="%",x="answers") + 
-  labs(title =paste("precision, recall by answers",failed_method));
+  labs(title =paste("precision, recall by answers",failing_methods[failed_method]));
+  #scale_x_continuous(limits=c(0,40));
+
 
 
 
