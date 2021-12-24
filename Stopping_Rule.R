59fd803f09033fb4fff084306dc881bc4e17bd80
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-12-25 00:02:11 +0100

    Update Stopping_Rule.R

--- Stopping_Rule.R
+++ Stopping_Rule.R
@@ -12,9 +12,10 @@ correspond respectively to the functions stop_by_total_budget and
 stop_by_arm_budget.
 
 The confidence-based stopping rule assumes a minimum threshold probability that
-the top arms will change. The change could be either their internal order (i.e.,
-distinct permutations of the same arms) or any of the arms will be replaced by
-one or more arms. The parameterization of these two types of confidence rules 
+the top arms will change. The change could be either (1) a reshuffling of the arms 
+(i.e., generating a new permutation with the same arms) or (2) a replacement of any of the
+top arms (i.e., one or more arms being supplanted by other arms w.r.t. their reward).
+The parameterization of these two types of confidence rules 
 require the number of top arms to consider and the threshold. The functions
 implementing these rules correspond to stop_confident_reshuffle and
 stop_confident_replacement
