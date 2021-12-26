45df7773e921371b711b5353e58619f9fa573a84
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-12-26 23:55:53 +0100

    Create Bandit_ActionElimination_BudgetBased.R

---
+++ Bandit_ActionElimination_BudgetBased.R
@@ -0,0 +1,9 @@
+"
+Action Elimination Bandit based on Budget
+
+Relies on pure exploration strategy.
+
+1- determine the event to eliminate an action  
+2- decide if we should stop when we achieve the minimum top-M arms or go until only one arm is left?
+
+"
