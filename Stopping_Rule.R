06e13d806f6dcc5aa2d9af387b5c867ac0372f9e
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-12-28 00:55:45 +0100

    Update Stopping_Rule.R

--- Stopping_Rule.R
+++ Stopping_Rule.R
@@ -19,18 +19,53 @@ The parameterization of these two types of confidence rules
 require the number of top arms to consider and the threshold. The functions
 implementing these rules correspond to stop_confident_reshuffle and
 stop_confident_replacement
-
 "
 
-" 
-Generates permutations 
+"Generates permutations of the elements of x
+source: https://stackoverflow.com/questions/11095992/generating-all-distinct-permutations-of-a-list-in-r
 "
-generate_permutations <- function(top_m,ranking){
-  return top_m_permutations
+generate_permutations <- function(x) {
+  if (length(x) == 1) {
+    return(x)
+  }
+  else {
+    res <- matrix(nrow = 0, ncol = length(x))
+    for (i in seq_along(x)) {
+      res <- rbind(res, cbind(x[i], Recall(x[-i])))
+    }
+    return(res)
+  }
 }
 
+"
+Compute distance in terms of samples from each permutation and 
+the current ranking_top. The distance is in how many sampled answers
+are necessary for the original become the permutations.
+"
+compute_min_max_distance <- function(permutation,current_top){
+  "the trick here is what is the minimum number of yes answers
+  necessary to be added to transform current_top -> permutation.
+  Can I use the Wasserstein distance for that?
+  
+  1- check which item change position. If it did not change, then keeps same number of answers
+  2- for the ones that changed position, find the lowest one
+  3- compute the difference between the changed position and the antecedent. Add difference +1
+  4- save the information of how many extra answers for that item
+  5- go the the next up in the ranking an repeat 3 until end of ranking
+  
+  
+  Note that these addition of answers assumes optimistically that all items have
+  equal chance of receiving a positive answer that has high accuracy. Realistically, 
+  we knwo that certain questions have lower chances than others of obtaining a 
+  positive and high accurate answer. However, this would make the algorithm 
+  more pessimistic, i.e., allow for ealier stopping. This is not what we are 
+  aiming at, i.e., we want min max solution. Miminum number of answers that would
+  cause a maximum change in the ranking.
+  "
+}
 
-"Determines if should stop or not given the remaining budget.
+"
+Determines if should stop or not given the remaining budget.
  budget: how many samples could still be drawn
  top: how many arms are considered at the top
  ranking: the current ranking of arms
