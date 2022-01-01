7eabc871ee471c30636b4cb1c8d9dea36c175c2d
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-01-01 23:56:59 +0100

    Update FalseDiscoveryRate.R

--- FalseDiscoveryRate.R
+++ FalseDiscoveryRate.R
@@ -2,6 +2,13 @@
 Compute the False Discovery Rate of multiple experiments
 "
 
+"https://strimmerlab.github.io/software/fdrtool/"
 install.packages("fdrtool")
 library(fdrtool)
+
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("onlineFDR")
 
