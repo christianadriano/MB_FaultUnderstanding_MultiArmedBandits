8930ca124a99757a5fee33403cbfe9e4b8bcd4f4
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-01-11 23:27:51 +0100

    Create Bandits_Top-m_Arm_Identification.R

---
+++ Bandits_Top-m_Arm_Identification.R
@@ -0,0 +1,12 @@
+"
+Top-m Arm identification 
+
+How to identify the top-m arms assuming some stopping rule.
+
+References:
+Russo, D, (2018), Simple bayesian algorithms for best arm identification
+Réda, C., Kaufmann, E., & Delahaye-Duriez, A., (2018), Top-m identification for linear bandits
+Réda, C., Tirinzoni, A., & Degenne, R. (2021). Dealing With Misspecification In Fixed-Confidence Linear Top-m Identification. Advances in Neural Information Processing Systems, 34.
+
+"
+
